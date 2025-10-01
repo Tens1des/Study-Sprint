9835df6292333af858c91bf94750d0dcdf806799
Tens1des <romankotov3213@gmail.com>
2025-10-01 20:30:06 +0300

    Initial Commit

---
+++ Study Sprint/Study_SprintApp.swift
@@ -0,0 +1,17 @@
+//
+//  Study_SprintApp.swift
+//  Study Sprint
+//
+//  Created by Рома Котов on 01.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct Study_SprintApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
